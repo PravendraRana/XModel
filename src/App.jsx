0bd4e42ec61ed7a236191a90b118d3a79c399509
PravendraRana <pravendra56rana@gmail.com>
2025-01-31 19:17:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import Form from "./components/form";
+import Modal from "./components/Modal";
+import { useState } from "react";
+
+function App() {
+  const [open, setOpen] = useState(false);
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>User Details Modal</h1>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            setOpen(true);
+          }}
+          style={{
+            padding: "0.5rem",
+            width: "10rem",
+            background: "rgb(0, 155, 255)",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",
+            color: "white",
+          }}
+        >
+          Open Form
+        </button>
+      </div>
+
+      <Modal open={open} onClose={onClose}>
+        <Form/>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,37 @@
+import "./Modal.css";
+import { useEffect } from "react";
+
+const Modal = ({ children, open, onClose }) => {
+  console.log("form", open);
+
+  useEffect(() => {
+    debugger;
+    const handleOutsideClick = (event) => {
+      if (!event.target.closest(".modal-content") && open) {
+        onClose(); // Call onClose function passed as prop to close the modal
+      }
+    };
+
+    // Add event listener when the modal is open
+    if (open) {
+      document.addEventListener("click", handleOutsideClick);
+    }
+
+    // Clean up event listener when component unmounts or modal closes
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [open, onClose]);
+
+  return (
+    <>
+      {open && (
+        <div className="modal active">
+          <div className="modal-content">{children}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
